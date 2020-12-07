e761b526c7ca7ee80ed41cc87dec96105189a3ae
wk <somnuk.wk@gmail.com>
2020-12-07 23:26:42 +0700

    Refactor

--- resource/Attribute.cs
+++ resource/Attribute.cs
@@ -1,22 +1,25 @@
 using System;
 
-[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
-public class ExcludeAttribute : Attribute {
-    public Type BaseType { set; get; }
-    public string[] Properties { set; get; }
+namespace Hello {
+    [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    public class ExcludeAttribute : Attribute {
+        public Type BaseType { set; get; }
+        public string[] Properties { set; get; }
 
-    public ExcludeAttribute(Type baseType, params string[] props) {
-        BaseType = baseType;
-        Properties = prop;
+        public ExcludeAttribute(Type baseType, params string[] props) {
+            BaseType = baseType;
+            Properties = prop;
+        }
     }
-}
 
-public class Student {
-    public string FirstName { set; get; }
-    public string LastName { set; get; }
-}
+    public class Student {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public int Age { set; get; }
+    }
 
-[Exclude(typeof(Student), "FirstName")]
-public class Student2 {
+    [Exclude(typeof(Student), "FirstName", "Age")]
+    public class Student2 {
 
+    }
 }
--- src/Attribute/Program.cs
+++ src/Attribute/Program.cs
@@ -6,38 +6,50 @@ using Microsoft.CodeAnalysis;
 using System.IO;
 using Microsoft.CodeAnalysis.Emit;
 using System.Reflection;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
-namespace Attribute {
-    class Program {
-        static void Main(string[] args) {
-            var source = File.ReadAllText("resource/Attribute.cs");
-            var defaultCompilationOptions =
-                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
-                        .WithOverflowChecks(true)
-                        .WithPlatform(Platform.X64)
-                        .WithOptimizationLevel(OptimizationLevel.Debug);
+var source = File.ReadAllText("resource/Attribute.cs");
+var defaultCompilationOptions =
+    new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+            .WithOverflowChecks(true)
+            .WithPlatform(Platform.X64)
+            .WithOptimizationLevel(OptimizationLevel.Debug);
 
-            var corelib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
-            var tree = CSharpSyntaxTree.ParseText(source);
-            var compilation = CSharpCompilation.Create("TestRoslyn.dll",
-                new SyntaxTree[] { tree }, new[] { corelib }, defaultCompilationOptions);
+var corelib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+var tree = CSharpSyntaxTree.ParseText(source);
+var compilation = CSharpCompilation.Create("TestRoslyn.dll",
+    new SyntaxTree[] { tree }, new[] { corelib }, defaultCompilationOptions);
 
-            var symbol = compilation.GetTypeByMetadataName("Student2");
+var symbol = compilation.GetTypeByMetadataName("Hello.Student2");
 
-            var excludeAttribute = symbol
-                .GetAttributes()
-                .FirstOrDefault(x => x.AttributeClass.Name == "ExcludeAttribute");
+var excludeAttribute = symbol
+    .GetAttributes()
+    .FirstOrDefault(x => x.AttributeClass.Name == "ExcludeAttribute");
 
-            var type = excludeAttribute.ConstructorArguments[0];
-            var props = excludeAttribute.ConstructorArguments[1];
+var baseType = excludeAttribute.ConstructorArguments[0].Value as INamedTypeSymbol;
+var properties = excludeAttribute.ConstructorArguments[1].Values;
 
-            var typeValue = type.Value as INamedTypeSymbol;
-            var members = typeValue.GetMembers()
-                .Where(x => x.Kind == SymbolKind.Property);
+var typeName = GetTypeName(baseType);
+var members = GetTypeMembers(baseType);
+var propertiesValue = GetPropertiesValue(properties);
 
-            foreach (var item in members) {
-                Console.WriteLine(item);
-            }
-        }
-    }
+Console.WriteLine("Type name = {0}", typeName);
+Console.WriteLine("Type members = {0}",
+    string.Join(", ", members.Select(x => $"{x.Name}({x.Type.Name})")));
+Console.WriteLine("Properties = {0}", string.Join(", ", propertiesValue));
+
+string[] GetPropertiesValue(ImmutableArray<TypedConstant> constants) {
+    return constants.Select(x => x.Value as string).ToArray();
+}
+
+string GetTypeName(INamedTypeSymbol symbol) {
+    return $"{symbol.ContainingNamespace}.{symbol.Name}";
+}
+
+static List<IPropertySymbol> GetTypeMembers(INamedTypeSymbol symbol) {
+    var members = symbol.GetMembers()
+        .Where(x => x.Kind == SymbolKind.Property)
+        .OfType<IPropertySymbol>();
+    return members.ToList();
 }
